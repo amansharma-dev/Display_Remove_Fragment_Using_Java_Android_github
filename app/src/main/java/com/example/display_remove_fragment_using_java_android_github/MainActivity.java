8caf9913657d2143c521fb4bc4e4e2cc732499e2
Aman Sharma <amansharma.11@hotmail.com>
2019-06-27 00:11:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/display_remove_fragment_using_java_android_github/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.display_remove_fragment_using_java_android_github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
